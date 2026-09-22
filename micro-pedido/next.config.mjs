4ed0ff959ce7d106f958a22b4fb447ed59f39805
annesilv4 <carolaynebarbozasilva@gmail.com>
2026-09-22 20:57:34 -0300

    feat: configura module federation no micro pedido

--- micro-pedido/next.config.mjs
+++ micro-pedido/next.config.mjs
@@ -1,6 +1,34 @@
+import { createRequire } from "node:module";
+
+const require = createRequire(import.meta.url);
+
+const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  webpack(config, options) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "pedido",
+
+        filename: "static/chunks/remoteEntry.js",
+
+        exposes: {
+          "./Order": "./components/Order.js",
+        },
+
+        shared: {},
+
+        extraOptions: {
+          exposePages: true,
+        },
+      }),
+    );
+
+    return config;
+  },
 };
 
 export default nextConfig;
